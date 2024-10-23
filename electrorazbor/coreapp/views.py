bfef0f8112704aa5ef88b68e81f7c94bb369a085
BoykoVik <gexar47@gmail.com>
2024-10-23 20:21:57 +0300

    links models

--- electrorazbor/coreapp/views.py
+++ electrorazbor/coreapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from products.models import Categories
 
 # Create your views here.
 def home(request):
     return render(request, 'coreapp/home.html', {
+        'models': Categories.objects.all(),
         'title': '',
         'description': '',
         'keywords': '',
