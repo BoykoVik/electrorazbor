a6d3aad051a98d3e0ea8ceb9ca10f5fb0140a0f7
BoykoVik <gexar47@gmail.com>
2024-10-25 17:40:53 +0300

    tp

--- electrorazbor/coreapp/models.py
+++ electrorazbor/coreapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Contacts(models.Model):
+    title = models.CharField(blank=False, max_length=180, verbose_name='Наименование')
+    text = models.CharField(blank=False, max_length=180, verbose_name='Текст (выводимый)')
+    link = models.CharField(blank=False, max_length=180, verbose_name='Ссылка', help_text='Например https://t.me/link')
+    code = models.CharField(blank=False, default='fa-phone', max_length=180, verbose_name='Код иконки', help_text='Например fa-shower https://fontawesome.com/v4/icon/question')
+    show_in_top = models.BooleanField(default=False, verbose_name='Выводить сверху')
+    show_in_bottom = models.BooleanField(default=False, verbose_name='Выводить снизу')
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+
+    def __str__(self):
+        return self.title
