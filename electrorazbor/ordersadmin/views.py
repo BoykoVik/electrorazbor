f34ed4cc8e18193e283f237c3e198c55c9fde679
BoykoVik <gexar47@gmail.com>
2025-04-19 20:28:57 +0300

    start order-detail

--- electrorazbor/ordersadmin/views.py
+++ electrorazbor/ordersadmin/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from products.models import Products, Orders, Obtains
 from coreapp.models import Callrequest
+from django.db.models import Sum, F
 # Create your views here.
 def orderslist(request):
     callreqs = Callrequest.objects.all()
-    orders = Orders.objects.all()
+    orders = Orders.objects.annotate(total_price=Sum(F('order__product__price') * F('order__count'))).all()
     return render(request, 'ordersadmin/orderslist.html', {
         'title': 'Список заказов',
         'description': 'админка',
@@ -14,8 +15,10 @@ def orderslist(request):
 
 def orderdetail(request, id):
     order = get_object_or_404(Orders, id = id)
+    obtains = Obtains.objects.filter(order = order.id)
     return render(request, 'ordersadmin/orderdetail.html', {
     'order': order,
+    'obtains': obtains,
     'title': f'Заказ #{order.id}',
     'description': 'админка',
     })
