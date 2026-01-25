8bad8c317d1a9103a98f5468ef274379b87d7de3
BoykoVik <gexar47@gmail.com>
2026-01-25 15:47:01 +0300

    swiper

--- electrorazbor/products/admin.py
+++ electrorazbor/products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Categories, Products, ProductsImages, CharacteristicsMiddleModel, Characteristics, Firms, Obtains, Orders
+from django.utils.safestring import mark_safe
 # Register your models here.
 
 class CharacteristicsMiddleModelInline(admin.StackedInline):
@@ -7,7 +8,14 @@ class CharacteristicsMiddleModelInline(admin.StackedInline):
 
 class ProductsImagesInline(admin.StackedInline):
     model = ProductsImages
+    readonly_fields = ('image_preview',)
 
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="100" height="100" style="object-fit: cover;" />')
+        return "Нет изображения"
+    image_preview.short_description = "Превью"
+    
 @admin.register(Firms)
 class FirmsAdmin(admin.ModelAdmin):
     list_display = ("name", "rang",)
@@ -24,12 +32,19 @@ class CategoriesAdmin(admin.ModelAdmin):
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "in_top", "show", "rang", "use_in_feed",)
+    list_display = ("id", "name", "price", "get_main_image", "in_top", "show", "rang", "use_in_feed",)
     list_editable = ("price", "in_top", "show", "rang", "use_in_feed",)
     list_filter = ("category", "show",)
     filter_horizontal = ("category", "like_what",) #для удобного добавления
     prepopulated_fields = {"slug": ("name", )}
     inlines =(CharacteristicsMiddleModelInline, ProductsImagesInline,)
+
+    def get_main_image(self, obj):
+        main_image = obj.productimages.first()
+        if main_image:
+            return mark_safe(f'<img src="{main_image.image.url}" width="50">')
+        return "Нет фото"
+    get_main_image.short_description = "Фото"
 
 
 @admin.register(Characteristics)
