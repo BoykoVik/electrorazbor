1021a20ee4de95bde432f3296d89d05233afed72
BoykoVik <gexar47@gmail.com>
2026-01-25 22:05:35 +0300

    blocks

--- electrorazbor/electrorazbor/settings.py
+++ electrorazbor/electrorazbor/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'coreapp.apps.CoreappConfig',
     'products.apps.ProductsConfig',
     'apiapp.apps.ApiappConfig',
+    'pagesapp.apps.PagesappConfig',
     'ordersadmin.apps.OrdersadminConfig',
     'django_cleanup', #для автоматичечкой чистки медиа
     'easy_thumbnails', #для миниатюр
--- electrorazbor/pagesapp/apps.py
+++ electrorazbor/pagesapp/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PagesappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pagesapp'
+    verbose_name = "в разработке"
